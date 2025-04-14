c40178f04cf64b2ec24642fee6a207916d6a3cc9
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2025-04-14 16:55:43 +0900

    fix sample 4th

--- src/app/Sample05.tsx
+++ src/app/Sample05.tsx
@@ -1,6 +1,6 @@
 export function Orange() {
   return (
-    <div className="m-4 flex min-h-screen flex-col items-center justify-center py-4">
+    <div className="m-2 flex min-h-screen flex-col items-center justify-center py-4">
       this is orange
     </div>
   );
